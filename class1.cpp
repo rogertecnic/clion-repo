882110f2e12791e338ba28a5919dbf93176975f6
rogertecnic <roger_tecnic@hotmail.com>
2017-06-25 18:08:40 -0300

    segundo commit, foi todos os arquivos agora

---
+++ class1.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   class1.cpp
+ * Author: rogerio
+ * 
+ * Created on 25 de Junho de 2017, 17:20
+ */
+
+#include "class1.h"
+
+class1::class1() {
+}
+
+class1::class1(const class1& orig) {
+}
+
+class1::~class1() {
+}
+
---
+++ main.cpp
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: rogerio
+ *
+ * Created on 24 de Junho de 2017, 22:35
+ */
+
+#include<iostream>
+
+using namespace std;
+
+
+static void imprime(string s){
+    cout <<s<<endl;
+}
+string s;
+
+/*
+ * funcao main
+ */
+int main(int argc, char** argv) {
+    imprime("birlll");
+    return 0;
+}
+
