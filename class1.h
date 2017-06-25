8fc384df9120164d4351cfab6a3c000f89a0c079
rogertecnic <roger_tecnic@hotmail.com>
2017-06-25 17:45:01 -0300

    primeiro commit pelo netbeans

---
+++ class1.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   class1.h
+ * Author: rogerio
+ *
+ * Created on 25 de Junho de 2017, 17:20
+ */
+
+#ifndef CLASS1_H
+#define CLASS1_H
+
+class class1 {
+public:
+    class1();
+    class1(const class1& orig);
+    virtual ~class1();
+private:
+
+};
+
+#endif /* CLASS1_H */
+
